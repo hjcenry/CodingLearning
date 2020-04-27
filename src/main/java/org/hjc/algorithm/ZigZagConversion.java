de01967cf36af1b0b249d4ba63a3efbd97ec5e0d
hjcenry <HJCenry921209>
2020-04-27 16:18:18 +0800

    ZigZagConversion

--- src/main/java/org/hjc/algorithm/ZigZagConversion.java
+++ src/main/java/org/hjc/algorithm/ZigZagConversion.java
@@ -59,23 +59,69 @@ public class ZigZagConversion {
     E C   I H   N
     T     S     G
 
-    A       A
-    A     A A
-    A   A   A
-    A A     A
-    A       A
+    思路：
+
+列数 0 1 2 3 4   5 6  7  8  9  10
+
+    1          11             21
+    2       10 12          20 22
+    3     9    13       19    23
+    4   8      14    18
+    5 7        15 17
+    6          16
 
     url:https://leetcode-cn.com/problems/zigzag-conversion/
      */
 
     public static void main(String[] args) {
-        System.out.println(new ZigZagConversion().convert("LEETCODEISHIRING", 3));
+        System.out.println(new ZigZagConversion().convert("LEETCODEISHIRING", 2));
+//        System.out.println(new ZigZagConversion().convert("A", 1));
+
+        /*
+        P   A   H   N
+        A P L S I I G
+        Y   I   R
+        */
+//        System.out.println(new ZigZagConversion().convert("PAYPALISHIRING", 3));
+
+        /*
+        P     I     N
+        A   L S   I G
+        Y A   H R
+        P     I
+        */
+//        System.out.println(new ZigZagConversion().convert("PAYPALISHIRING", 4));
     }
 
     public String convert(String s, int numRows) {
-        // 3:0 1 2 3 - 1
-        // 4:0 1 2 3 - 2 1
-        // 5:0 1 2 3 4 - 3 2 1
-        return null;
+        if (numRows == 1) {
+            return s;
+        }
+        int len = s.length();
+        // 新的字符数组
+        char[] newStrChars = new char[len];
+        int charIndex = 0;
+        // 总列数
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < len; col = col + numRows - 1) {
+                // 根据列数获取元素下标
+                int strIndex = 2 * col + row;
+
+                if (row != 0 && row != numRows - 1 && col > 0) {
+                    // 非首尾两行先取中间的数,当前列的第一行的index-行数
+                    int midIndex = col * 2 - row;
+                    if (midIndex >= len) {
+                        break;
+                    }
+                    newStrChars[charIndex++] = s.charAt(midIndex);
+                }
+                // 取竖线列上的数
+                if (strIndex >= len) {
+                    break;
+                }
+                newStrChars[charIndex++] = s.charAt(strIndex);
+            }
+        }
+        return String.copyValueOf(newStrChars, 0, len);
     }
 }
