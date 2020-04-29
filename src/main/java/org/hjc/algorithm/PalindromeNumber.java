8529c1a70409c926f537e30970990f90aa92636e
hjcenry <HJCenry921209>
2020-04-29 17:14:47 +0800

    PalindromeNumber

---
+++ src/main/java/org/hjc/algorithm/PalindromeNumber.java
@@ -0,0 +1,66 @@
+package org.hjc.algorithm;
+
+/**
+ * 回文数
+ *
+ * @author 何金成
+ * @date 2020/4/29 17:10
+ */
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(new PalindromeNumber().isPalindrome(121));
+        System.out.println(new PalindromeNumber().isPalindrome(-121));
+        System.out.println(new PalindromeNumber().isPalindrome(10));
+        System.out.println(new PalindromeNumber().isPalindrome(157565751));
+        System.out.println(new PalindromeNumber().isPalindrome(157565752));
+    }
+
+    /*
+    Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+    Example 1:
+
+    Input: 121
+    Output: true
+    Example 2:
+
+    Input: -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+    Example 3:
+
+    Input: 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+    Follow up:
+
+    Coud you solve it without converting the integer to a string?
+
+    判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+    示例 1:
+
+    输入: 121
+    输出: true
+    示例 2:
+
+    输入: -121
+    输出: false
+    解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+    示例 3:
+
+    输入: 10
+    输出: false
+    解释: 从右向左读, 为 01 。因此它不是一个回文数。
+    进阶:
+
+    你能不将整数转为字符串来解决这个问题吗？
+
+    url:https://leetcode-cn.com/problems/palindrome-number/
+     */
+
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+}
