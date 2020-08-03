7530d271ff43bae5a7f463744059d696ee2314d9
hjcenry <HJCenry921209>
2020-08-03 18:54:48 +0800

    RemoveNthNodeFromEndOfList.java

--- src/main/java/org/hjc/algorithm/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/hjc/algorithm/RemoveNthNodeFromEndOfList.java
@@ -83,6 +83,22 @@ public class RemoveNthNodeFromEndOfList {
         preNode.next = removeIndex == index ? null : removeNode.next;
         return head;
     }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre, end = pre;
+        while (n != 0) {
+            start = start.next;
+            n--;
+        }
+        while (start.next != null) {
+            start = start.next;
+            end = end.next;
+        }
+        end.next = end.next.next;
+        return pre.next;
+    }
 }
 
 class ListNode {
