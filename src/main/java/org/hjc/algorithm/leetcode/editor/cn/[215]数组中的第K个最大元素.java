c171b999401c7692c6a4aad92861eada8c18f331
hjcenry <hejincheng1209@163.com>
2023-11-14 19:43:31 +0800

    算法题

--- src/main/java/org/hjc/algorithm/leetcode/editor/cn/[215]数组中的第K个最大元素.java
+++ src/main/java/org/hjc/algorithm/leetcode/editor/cn/[215]数组中的第K个最大元素.java
@@ -38,43 +38,34 @@ import java.util.List;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
-//        List<Integer> numList = new ArrayList<>();
-//        for (int num : nums) {
-//            numList.add(num);
-//        }
-//        return quickSelect(numList, k);
-    }
-
-    private int quickSelect(List<Integer> nums, int k) {
-        // 随机选择基准数
-        int pivotIndex = 0;
-        int pivot = nums.get(pivotIndex);
-        // 将大于、小于、等于 pivot 的元素划分至 big, small, equal 中
-        List<Integer> big = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> small = new ArrayList<>();
-        for (int num : nums) {
-            if (num > pivot) {
-                big.add(num);
-            } else if (num < pivot) {
-                small.add(num);
-            } else {
-                equal.add(num);
+    int quickselect(int[] nums, int l, int r, int k) {
+        if (l == r) {
+            return nums[k];
+        }
+        int x = nums[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do {
+                i++;
+            } while (nums[i] < x);
+            do {
+                j--;
+            } while (nums[j] > x);
+            if (i < j) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
             }
         }
-        // 第 k 大元素在 big 中，递归划分
-        if (k <= big.size()) {
-            return quickSelect(big, k);
+        if (k <= j) {
+            return quickselect(nums, l, j, k);
+        } else {
+            return quickselect(nums, j + 1, r, k);
         }
-        // 第 k 大元素在 small 中，递归划分
-        if (nums.size() - small.size() < k) {
-            return quickSelect(small, k - nums.size() + small.size());
-        }
-        // 第 k 大元素在 equal 中，直接返回 pivot
-        return pivot;
+    }
+
+    public int findKthLargest(int[] _nums, int k) {
+        int n = _nums.length;
+        return quickselect(_nums, 0, n - 1, n - k);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
