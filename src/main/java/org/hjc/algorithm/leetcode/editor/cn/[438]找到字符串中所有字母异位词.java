f3d7f7e541fce7b5bd20c85ccc37dbc7583d56ca
hjcenry <hejincheng1209@163.com>
2023-11-16 17:07:11 +0800

    算法

---
+++ src/main/java/org/hjc/algorithm/leetcode/editor/cn/[438]找到字符串中所有字母异位词.java
@@ -0,0 +1,66 @@
+//给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+//
+// 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: s = "cbaebabacd", p = "abc"
+//输出: [0,6]
+//解释:
+//起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+//起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+//
+//
+// 示例 2:
+//
+//
+//输入: s = "abab", p = "ab"
+//输出: [0,1,2]
+//解释:
+//起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+//起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+//起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= s.length, p.length <= 3 * 10⁴
+// s 和 p 仅包含小写字母
+//
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 1327 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] need = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            need[p.charAt(i) - 'a']++;
+        }
+        int start = 0, end = 0;
+        int[] window = new int[26];
+        List<Integer> ans = new ArrayList<Integer>();
+        while (end < s.length()) {
+            window[s.charAt(end) - 'a']++;
+            if (end - start + 1 == p.length()) {
+                if (Arrays.equals(window, need)) {
+                    ans.add(start);
+                }
+                window[s.charAt(start) - 'a']--;
+                start++;
+            }
+            end++;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
